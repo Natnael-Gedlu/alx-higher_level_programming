862bf19d7f281e7b9bb62288d02b4f4a65d0c68c
Natnael-Gedlu <nategd800@gmail.com>
2023-08-11 17:20:35 +0300

    added 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif num_arguments == 1:
         print("1 argument:")
-        print("1:", sys.argv[0])
+        print("1:", sys.argv[1])
     else:
         print(f"{num_arguments} arguments:")
-        for i, arg in enumerate(sys.argv, start=1):
+        for i, arg in enumerate(sys.argv[1:], start=1):
             print(f"{i}: {arg}")
