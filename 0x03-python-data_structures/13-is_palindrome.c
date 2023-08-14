aa6c2d7ceb00cb85a49a1252f378056d5c794d38
Natnael-Gedlu <nategd800@gmail.com>
2023-08-14 18:36:48 +0300

    added 13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+int is_palindrome(listint_t **head)
+{
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+    listint_t *temp;
+
+    if (*head == NULL || (*head)->next == NULL) {
+        return 1;
+    }
+
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+        temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
+    }
+
+    if (fast != NULL) {
+        slow = slow->next;
+    }
+
+    while (slow != NULL) {
+        if (slow->n != prev->n) {
+            return 0;
+        }
+        slow = slow->next;
+        prev = prev->next;
+    }
+
+    return 1;
+}
