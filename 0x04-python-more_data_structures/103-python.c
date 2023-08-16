7865f3da304faa6406c8044305b0f272db95cb38
Natnael-Gedlu <nategd800@gmail.com>
2023-08-16 18:44:41 +0300

    added Q-103

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,49 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p)
+{
+    Py_ssize_t size, i;
+    PyObject *item;
+
+    size = PyList_Size(p);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %zd\n", size);
+    printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
+
+void print_python_bytes(PyObject *p)
+{
+    Py_ssize_t size, i;
+    char *str;
+
+    printf("[.] bytes object info\n");
+    if (!PyBytes_Check(p))
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    size = PyBytes_Size(p);
+    str = PyBytes_AsString(p);
+
+    printf("  size: %zd\n", size);
+    printf("  trying string: %s\n", str);
+
+    if (size > 10)
+        size = 10;
+
+    printf("  first %zd bytes: ", size);
+    for (i = 0; i < size; i++)
+    {
+        printf("%02x", str[i] & 0xff);
+        if (i < size - 1)
+            printf(" ");
+    }
+    printf("\n");
+}
