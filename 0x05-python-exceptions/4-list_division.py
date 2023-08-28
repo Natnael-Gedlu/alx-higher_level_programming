d885823238bb9038aafd7a3c7d27fc7f9bc6aa68
Natnael-Gedlu <nategd800@gmail.com>
2023-08-28 18:26:05 +0300

    broken line 10 so that it fits within the recommended line length

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,8 @@ def list_division(my_list_1, my_list_2, list_length):
             try:
                 value_1 = my_list_1[i]
                 value_2 = my_list_2[i]
-                if isinstance(value_1, (int, float)) and isinstance(value_2, (int, float)):
+                if (isinstance(value_1, (int, float)) and
+                        isinstance(value_2, (int, float))):
                     if value_2 != 0:
                         result.append(value_1 / value_2)
                     else:
