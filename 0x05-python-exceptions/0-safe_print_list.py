606e3b3a96b2240694a95708be42c4620a36234e
Natnael-Gedlu <nategd800@gmail.com>
2023-08-28 18:44:05 +0300

    code corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    try:
-        while count < x:
-            print(my_list[count], end=' ')
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
             count += 1
-    except IndexError:
-        break
-    finally:
-        print()
+        except IndexError:
+            break
+    print("")
     return count
