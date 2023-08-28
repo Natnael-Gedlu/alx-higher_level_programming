3b2e477fc40b81c92a19bdeb411b933eaca77344
Natnael-Gedlu <nategd800@gmail.com>
2023-08-28 17:05:26 +0300

    added 0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
-        i = 0
-        for element in my_list:
-            if i < x:
-                print(element, end=' ')
-                i += 1
-        print()
-    except:
+        while count < x:
+            print(my_list[count], end=' ')
+            count += 1
+    except IndexError:
         pass
+    finally:
+        print()
 
-    return i
+    return count
