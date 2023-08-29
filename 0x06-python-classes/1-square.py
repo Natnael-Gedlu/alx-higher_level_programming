350a8c2e0629c7cd192131574e14afdd9d106dae
Natnael-Gedlu <nategd800@gmail.com>
2023-08-29 18:29:03 +0300

    added 1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This class defines a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a square with the given size.
+
+        :param size: The size of the square's sides.
+        """
+        self.__size = size
