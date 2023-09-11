51ca265ad9d971f788c54092abceceb9a54ee92f
Natnael-Gedlu <nategd800@gmail.com>
2023-09-11 19:07:28 +0300

    python-inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Defines a class MyInt that inherits from int.
+"""
+
+class MyInt(int):
+    """
+    A custom integer class that changes the behavior of equality operators.
+    """
+    def __eq__(self, value):
+        """
+        Check if the instance is not equal to the given value.
+        """
+        return self.real != value
+
+    def __ne__(self, value):
+        """
+        Check if the instance is equal to the given value.
+        """
+        return self.real == value
