3c6ebfea052de4663b870c26e77a3d1db285f7b2
Natnael-Gedlu <nategd800@gmail.com>
2023-09-13 18:52:07 +0300

    Python-Input/Output

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,14 +3,17 @@
 Defines a text insertion to a file function.
 """
 
+
 def append_after(filename="", search_string="", new_string=""):
     """
     Inserts a new string after a specific search string in a text file.
 
     Parameters:
         filename (str): The name of the text file to add on.
-        search_string (str): The string to search for within the file.
-        new_string (str): The string to insert after the `search_string` if found.
+        search_string (str): The string to search for
+        within the file.
+        new_string (str): The string to insert after the
+        `search_string` if found.
 
     """
     text = ""
