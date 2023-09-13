bbfd6fd396f64d0a96f1e05d12b2984276447bf0
Natnael-Gedlu <nategd800@gmail.com>
2023-09-13 17:42:24 +0300

    Python-Input/Output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,5 +9,5 @@ def save_to_json_file(my_obj, filename):
     """
     function that saves to JSON file.
     """
-    with open(filename, 'w') as file:
+    with open(filename, "w") as file:
         json.dump(my_obj, file)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -10,4 +10,4 @@ def load_from_json_file(filename):
     creates an Object from a “JSON file”.
     """
     with open(filename) as file:
-        json.load(file)
+        return json.load(file)
