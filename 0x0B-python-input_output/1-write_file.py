33f3fe4e263be82235203ebe122d6769ef3ecac9
Natnael-Gedlu <nategd800@gmail.com>
2023-09-13 16:52:15 +0300

    Python-Input/Output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines a function that writes a string to a text file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    function that returns the number of characters written.
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        print(file.write(text), end="")
