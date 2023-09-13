604af5a2398044fb452c22bbe7f8a1d0b6b6f971
Natnael-Gedlu <nategd800@gmail.com>
2023-09-13 16:39:43 +0300

    Python-Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 Defines a function that reads a text file and prints it to stdout.
 """
@@ -9,4 +9,4 @@ def read_file(filename=""):
     function that prints out file contents.
     """
     with open(filename, 'r', encoding="utf-8") as file:
-        print(file.read, end='')
+        print(file.read(), end='')
