b53138b04a51b09bbd735e9ad1284187a7efd1f9
Natnael-Gedlu <nategd800@gmail.com>
2023-09-18 17:20:01 +0300

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,5 +107,37 @@ class Rectangle(Base):
         """
         Display the Rectangle instance with the character #.
         """
+        for _ in range(self.__y):
+            print()
+
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """
+        Return a string representation of the Rectangle.
+        """
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """
+        Update attributes of the Rectangle based on arguments.
+
+        Args:
+            *args: Variable number of arguments in the following order:
+                1st argument: id attribute
+                2nd argument: width attribute
+                3rd argument: height attribute
+                4th argument: x attribute
+                5th argument: y attribute
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
