85b6e8b1b9162af0ef8edf28518fb577887eb6ed
Natnael-Gedlu <nategd800@gmail.com>
2023-10-18 17:35:57 +0300

    SQL_more_queries

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT t.title, SUM(r.rate) AS rating
+FROM tv_shows t
+LEFT JOIN tv_show_ratings r ON r.show_id = t.id
+GROUP BY t.title
+ORDER BY rating DESC;
+
