246f01b66813f8e2bc7db7fb4df1360a0d753856
Natnael-Gedlu <nategd800@gmail.com>
2023-11-20 17:07:07 +0300

    Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that lists all states from the database.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQL.connect(host="localhost", port=3306, user=sys.argv[1],
+                       passwd=sys.argv[2], db=sys.argv[3])
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
